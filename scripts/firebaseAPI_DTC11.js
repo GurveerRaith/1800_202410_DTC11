087f48f4ecfdc84b5ed4dc22be3819992e5836fe
Ardacar <Ardi.Daeizadeh@gmail.com>
2024-03-03 16:04:38 -0800

    Add authentication.js and login.html

--- scripts/firebaseAPI_DTC11.js
+++ scripts/firebaseAPI_DTC11.js
@@ -1,14 +1,18 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-  apiKey: "AIzaSyA2KuJgewEB-Wr4IuVFMKaJiypsspPuo1k",
-  authDomain: "comp1800-d6473.firebaseapp.com",
-  projectId: "comp1800-d6473",
-  storageBucket: "comp1800-d6473.appspot.com",
-  messagingSenderId: "941186638635",
-  appId: "1:941186638635:web:212c5413a423a6a85adb6e"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlOVeNNPdHhngKM8XHkPjuJhzly05f5Lc",
+  authDomain: "dtc11-70f41.firebaseapp.com",
+  projectId: "dtc11-70f41",
+  storageBucket: "dtc11-70f41.appspot.com",
+  messagingSenderId: "384427724291",
+  appId: "1:384427724291:web:ffd57db5d4109fda294836"
 };
+
 
 //--------------------------------------------
 // initialize the Firebase app
@@ -17,3 +21,4 @@ var firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const storage = firebase.storage();
+
