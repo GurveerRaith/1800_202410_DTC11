b6713cc6f6b486f6dc229efb9286cfd50ab45f36
Melissa Shao <mshao8@my.bcit.ca>
2024-03-04 22:32:18 -0800

    Debug the login page and the CSS in main page

--- scripts/firebaseAPI_DTC11.js
+++ scripts/firebaseAPI_DTC11.js
@@ -4,13 +4,22 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDlOVeNNPdHhngKM8XHkPjuJhzly05f5Lc",
+//   authDomain: "dtc11-70f41.firebaseapp.com",
+//   projectId: "dtc11-70f41",
+//   storageBucket: "dtc11-70f41.appspot.com",
+//   messagingSenderId: "384427724291",
+//   appId: "1:384427724291:web:ffd57db5d4109fda294836"
+// };
+
 var firebaseConfig = {
-  apiKey: "AIzaSyDlOVeNNPdHhngKM8XHkPjuJhzly05f5Lc",
-  authDomain: "dtc11-70f41.firebaseapp.com",
-  projectId: "dtc11-70f41",
-  storageBucket: "dtc11-70f41.appspot.com",
-  messagingSenderId: "384427724291",
-  appId: "1:384427724291:web:ffd57db5d4109fda294836"
+  apiKey: "AIzaSyA2KuJgewEB-Wr4IuVFMKaJiypsspPuo1k",
+  authDomain: "comp1800-d6473.firebaseapp.com",
+  projectId: "comp1800-d6473",
+  storageBucket: "comp1800-d6473.appspot.com",
+  messagingSenderId: "941186638635",
+  appId: "1:941186638635:web:212c5413a423a6a85adb6e"
 };
 
 
