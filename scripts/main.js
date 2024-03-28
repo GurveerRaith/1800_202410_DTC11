6287f168fb9b567ed42d1cb6184a51f299cd3bf9
Melissa Shao <mshao8@my.bcit.ca>
2024-03-27 22:13:46 -0700

    Debug the new users can store user data to the database

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,22 @@
-function getNameFromAuth() {
+// 3.3 - Read the user’s name from Firestore and display it on the main page using .get() :
+function insertNameFromFirestore() {
+  // Check if the user is logged in:
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      // Do something for the currently logged-in user here: 
-      console.log(user.uid); //print the uid in the browser console
-      console.log(user.displayName);  //print the user name in the browser console
-
-      userName = user.displayName;
-      //method #1:  insert with JS
-      document.getElementById("name-goes-here").innerText = userName;
-
+      console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+      currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+      currentUser.get().then(userDoc => {
+        // Get the user name
+        let userName = userDoc.data().name;
+        console.log(userName);
+        //$("#name-goes-here").text(userName); // jQuery
+        document.getElementById("name-goes-here").innerText = userName;
+      })
     } else {
-      // No user is signed in.
-      console.log("No user is logged in");
+      console.log("No user is logged in."); // Log a message when no user is logged in
     }
   })
-
 }
-getNameFromAuth(); //run the function
 
+insertNameFromFirestore();
 
