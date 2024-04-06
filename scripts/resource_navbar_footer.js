b127604cfa3b4ecad9376ba287f71e48573f3211
Melissa Shao <mshao8@my.bcit.ca>
2024-04-06 11:16:08 -0700

    Clear up the code in resource_navbar_footer.js file

--- scripts/resource_navbar_footer.js
+++ scripts/resource_navbar_footer.js
@@ -18,20 +18,3 @@ function loadSkeleton() {
   });
 }
 loadSkeleton(); //invoke the function
-
-
-// // Function to read the quote of the day from the Firestore "quotes" collection
-// // Input param is the String representing the day of the week, aka, the document name
-// function readQuote(userID) {
-//   db.collection("users").doc(userID)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
-//     .onSnapshot(dayDoc => {                                                               //arrow notation
-//       console.log("current document data: " + dayDoc.data());                          //.data() returns data object
-//       document.getElementById("user-name-goes-here").innerHTML = dayDoc.data().name;      //using javascript to display the data on the right place
-
-//       //Here are other ways to access key-value data fields
-//       //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
-//       //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
-//       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
-//     })
-// }
-// readQuote("FsLx1X5DM6SvQS5q8fjsIQNiLAc2");        //calling the function
